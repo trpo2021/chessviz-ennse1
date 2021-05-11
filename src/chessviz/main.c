3407b82cefd9157dc6a8eaf80ae214eb0e043fd2
Egor <75725159+ennse1@users.noreply.github.com>
2021-05-11 14:54:01 +0700

    Update main.c

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,59 +1,75 @@
-#include <libchessviz/chess.h>
-#include <libchessviz/chess_read.h>
-#include <libchessviz/chessboard_create.h>
-#include <libchessviz/chessboard_print_plain.h>
+  
+#include "libchessviz/board.h"
+#include "libchessviz/board_print_plain.h"
+#include "libchessviz/board_read.h"
+#include "libchessviz/move.h"
 #include <stdio.h>
-#include <string.h>
 
-int main(int argc, char** argv)
+int main()
 {
-    FILE* f;
-    char* filename;
+    char desk[9][9];
+    char chessMove[11];
+    int check;
+    check = 0;
 
-    Moves moves;
-    moves.count = 0;
-    Chessboard chessboard;
-    char string[(CHESSBOARD_SIZE + 1) * (CHESSBOARD_SIZE + 1) * 2 + 1];
-    char inputString[64];
+    boardFill(desk);
+    boardPrint(desk);
+    printf("Move example: 'e2-e4'\nType 0 to exit program");
 
-    ParseError parseError = {.errtype = ParseErrorTypeNone};
-    MoveError moveError = {.errtype = MoveErrorTypeNone};
-
-    if (argc == 1) {
-        printf("Usage: chessviz <filename>");
-        return 0;
-    }
-
-    filename = argv[1];
-    f = fopen(filename, "r");
-
-    while (fgets(inputString, 64, f) != NULL) {
-        printf("%s", inputString);
-        if (parseStep(inputString, &moves, &parseError)) {
-            printf("\n%s", parseError.errstr);
-            return 0;
+    while (1) {
+        printf("\nType a move: ");
+        boardRead(chessMove);
+        check = move(desk, chessMove);
+        if (check == WRONGINPUT) {
+            printf("\nInput data fail\n");
+            break;
         }
-    }
-    createChessboard(
-            &chessboard,
-            "rnbqkbnr"
-            "pppppppp"
-            "        "
-            "        "
-            "        "
-            "        "
-            "PPPPPPPP"
-            "RNBQKBNR");
-    int errnum = 0;
-    for (int i = 0; i < moves.count; i++) {
-        errnum = doMove(moves, i, &chessboard, &moveError);
-        if (errnum) {
-            printf("\n%s\n", moveError.errstr);
-            return 1;
+        if (check == EXITSUCCESS) {
+            printf("\n\nExiting program\n");
+            break;
         }
-        chessboardToString(&chessboard, string);
 
-        printf("\n%s", string);
+        if (check == PAWNERROR) {
+            printf("\n\nWrong figure chosen. Must be Pawn\n");
+            break;
+        }
+
+        if (check == KINGERROR) {
+            printf("\n\nWrong figure chosen. Must be King\n");
+            break;
+        }
+
+        if (check == BORDERLIMIT) {
+            printf("\n\nStep out of border\n");
+            break;
+        }
+
+        if (check == QUEENERROR) {
+            printf("\n\nWrong figure chosen. Must be Queen\n");
+            break;
+        }
+
+        if (check == ROOKERROR) {
+            printf("\n\nWrong figure chosen. Must be Rook\n");
+            break;
+        }
+
+        if (check == KNIGHTERROR) {
+            printf("\n\nWrong figure chosen. Must be knight\n");
+            break;
+        }
+
+        if (check == BISHOPERROR) {
+            printf("\n\nWrong figure chosen. Must be Bishop\n");
+            break;
+        }
+
+        if (check == CHECKMATE) {
+            printf("\n\nCheckmate, well done!\n");
+            break;
+        }
+
+        boardPrint(desk);
     }
 
     return 0;
